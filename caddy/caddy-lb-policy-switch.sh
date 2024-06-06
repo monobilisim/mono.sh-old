c6a0cfe7fee14eccc0bd377009036fd5d9dd3f17
Kreato (Work) <kreato@kreato.dev>
2024-06-06 10:53:53 +0300

    caddy-lb-policy-switch: Fix all shellcheck warnings, don't send the same config as before

--- caddy/caddy-lb-policy-switch.sh
+++ caddy/caddy-lb-policy-switch.sh
@@ -8,13 +8,14 @@ fi
 
 function debug() {
     if [[ "$DEBUG" -eq 1 ]]; then
-        echo "debug: $@"
+        echo "debug: $1"
     fi
 }
 
 function alarm() {
     if [ "$SEND_ALARM" == "1" ]; then
         if [ -z "$ALARM_WEBHOOK_URLS" ]; then
+            #shellcheck disable=SC2153
             curl -fsSL -X POST -H "Content-Type: application/json" -d "{\"text\": \"$1\"}" "$ALARM_WEBHOOK_URL" 1>/dev/null
         else
             for webhook in "${ALARM_WEBHOOK_URLS[@]}"; do
@@ -50,14 +51,16 @@ function identify_request() {
         debug "checking server: $SERVER"
 
         # Identify the routes
-        LENGTH="$(curl -s -u $USERNAME_PASSWORD $URL/config/apps/http/servers/${SERVER:?}/routes | jq length)"
+        LENGTH="$(curl -s -u "$USERNAME_PASSWORD" "$URL"/config/apps/http/servers/"${SERVER:?}"/routes | jq length)"
         
         debug "Routes: $LENGTH"
 
         for route in $(seq 0 $((LENGTH-1))); do
-            export REQ_URL="$URL/config/apps/http/servers/${SERVER:?}/routes/$route"
-            export REQ="$(curl -u "$USERNAME_PASSWORD" -s $REQ_URL)"
-            
+            REQ_URL="$URL/config/apps/http/servers/${SERVER:?}/routes/$route"
+            REQ="$(curl -u "$USERNAME_PASSWORD" -s "$REQ_URL")"
+            export REQ
+            export REQ_URL
+
             if (echo "$REQ" | jq -r '.match[].host | join(" ")' 2> /dev/null | grep -qw "$URL_TO_FIND"); then
                 echo "Match found, route: $route, server: $SERVER"
                 change_upstreams "$1" "$2"
@@ -70,7 +73,7 @@ function change_upstreams() {
     echo "Changing upstreams"
     case $1 in
         first)
-            curl -u "$USERNAME_PASSWORD" -X PATCH -H "Content-Type: application/json" -d "$(echo "$REQ" | jq --arg SRVNAME "$2" -cMr '
+            REQ_TO_SEND="$(echo "$REQ" | jq --arg SRVNAME "$2" -cMr '
                 .handle[] |= (
                   .routes[] |= (
                     .handle[] |= (
@@ -87,17 +90,27 @@ function change_upstreams() {
                     )
                   )
                 )
-                ')" "$REQ_URL"
-            
-            if [ $? -eq 0 ]; then
-                alarm "[Caddy lb-policy Switch] [$URL] [$URL_TO_FIND] [:check:] Switched lb_policy to first_$2"
+                ')"
+
+            if [[ "$REQ_TO_SEND" == "$REQ" ]]; then
+                echo "No changes needed as the upstreams are already in the first_$2 order"
+                if [[ "$VERBOSE" -eq 1 ]]; then
+                    alarm "[Caddy lb-policy Switch] [$URL] [$URL_TO_FIND] [:check:] No changes needed as the upstreams are already in the first_$2 order"
+                fi
+                return
             else
-                alarm "[Caddy lb-policy Switch] [$URL] [$URL_TO_FIND] [:red_circle:] Failed to switch lb_policy to first_$2"
+                echo "Sending request to change upstreams"
+                
+                if curl -u "$USERNAME_PASSWORD" -X PATCH -H "Content-Type: application/json" -d "$REQ_TO_SEND" "$REQ_URL" 2> /tmp/caddy-lb-policy-switch-error.log; then
+                    alarm "[Caddy lb-policy Switch] [$URL] [$URL_TO_FIND] [:check:] Switched upstreams to first_$2"
+                else
+                    alarm "[Caddy lb-policy Switch] [$URL] [$URL_TO_FIND] [:red_circle:] Failed to switch upstreams to first_$2\nError log: \`\`\`\n$(cat /tmp/caddy-lb-policy-switch-error.log)\n\`\`\`"
+                fi
+
             fi
-            
             ;;
         round_robin | ip_hash)
-            curl -u "$USERNAME_PASSWORD" -X PATCH -H "Content-Type: application/json" -d "$(echo "$REQ" | jq --arg LB_POLICY "$1" -cMr '
+            REQ_TO_SEND="$(echo "$REQ" | jq --arg LB_POLICY "$1" -cMr '
                 .handle[] |= (
                   .routes[] |= (
                     .handle[] |= (
@@ -108,14 +121,22 @@ function change_upstreams() {
                     )
                   )
                 )
-                ')" "$REQ_URL"
+                ')"
 
-            if [ $? -eq 0 ]; then
-                alarm "[Caddy lb-policy Switch] [$URL] [$URL_TO_FIND] [:check:] Switched lb_policy to $1"
+            if [[ "$REQ_TO_SEND" == "$REQ" ]]; then
+                echo "No changes needed as the upstreams are already in the $1 order"
+                if [[ "$VERBOSE" -eq 1 ]]; then
+                    alarm "[Caddy lb-policy Switch] [$URL] [$URL_TO_FIND] [:check:] No changes needed as the upstreams are already in the $1 order"
+                fi
+                return
             else
-                alarm "[Caddy lb-policy Switch] [$URL] [$URL_TO_FIND] [:red_circle:] Failed to switch lb_policy to $1"
+                echo "Sending request to change lb_policy to $1"
+                if curl -u "$USERNAME_PASSWORD" -X PATCH -H "Content-Type: application/json" -d "$REQ_TO_SEND" "$REQ_URL" 2> /tmp/caddy-lb-policy-switch-error.log; then
+                    alarm "[Caddy lb-policy Switch] [$URL] [$URL_TO_FIND] [:check:] Switched lb_policy to $1"
+                else
+                    alarm "[Caddy lb-policy Switch] [$URL] [$URL_TO_FIND] [:red_circle:] Failed to switch lb_policy to $1\nError log: \`\`\`\n$(cat /tmp/caddy-lb-policy-switch-error.log)\n\`\`\`"
+                fi
             fi
-
             ;;
         *)
             echo "Invalid load balancing command"
@@ -132,6 +153,7 @@ fi
 for conf in /etc/glb/*.conf; do
     [ ! -f "$conf" ] && continue
     
+    #shellcheck disable=SC1090
     . "$conf"
     
     echo "---------------------------------"
@@ -148,10 +170,10 @@ for conf in /etc/glb/*.conf; do
     fi
 
 
-    for URL_UP in ${CADDY_API_URLS[@]}; do
+    for URL_UP in "${CADDY_API_URLS[@]}"; do
         URL="${URL_UP#*@}"
         USERNAME_PASSWORD="${URL_UP%%@*}"
-        for URL_TO_FIND in ${CADDY_SERVERS[@]}; do
+        for URL_TO_FIND in "${CADDY_SERVERS[@]}"; do
             echo '---------------------------------'
             echo "Checking '$URL_TO_FIND' on '$URL'"
             identify_request "$1" "$2"
